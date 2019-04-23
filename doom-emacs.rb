18c4d8d0a536323c557c0e959bd0df177fa9e934
Jeff Parsons <parsoj@gmail.com>
2019-04-22 19:24:48 -0700

    fix dependency option flags

--- doom-emacs.rb
+++ doom-emacs.rb
@@ -6,7 +6,7 @@ class DoomEmacs < Formula
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   version "1.0"
 
-  depends_on "emacs-plus --with-no-titlebar"
+  depends_on "emacs-plus" => "with-no-titlebar"
 
   def install
     system "git clone -b develop https://github.com/hlissner/doom-emacs.git ~/.emacs.d"
