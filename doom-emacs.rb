b5056b81acf83f30b35e63d5fa0c9e9a48431934
Jeff Parsons <parsoj@gmail.com>
2019-04-22 19:20:59 -0700

    add a version

--- doom-emacs.rb
+++ doom-emacs.rb
@@ -4,6 +4,7 @@ class DoomEmacs < Formula
 
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  version "1.0"
 
   depends_on "emacs-plus --with-no-titlebar"
 
