39354039fecf508ba21d26bb19c06faed4b4fe26
Jeff Parsons <parsoj@gmail.com>
2019-04-22 19:30:40 -0700

    fix dependency path

--- doom-emacs.rb
+++ doom-emacs.rb
@@ -6,7 +6,7 @@ class DoomEmacs < Formula
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   version "1.0"
 
-  depends_on "emacs-plus" => "with-no-titlebar"
+  depends_on "d12frosted/emacs-plus/emacs-plus" => ["with-no-titlebar", "without-spacemacs-icon", "with-no-frame-refocus", "with-modern-icon"]
 
   def install
     system "git clone -b develop https://github.com/hlissner/doom-emacs.git ~/.emacs.d"
