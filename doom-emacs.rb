cd694d217d812d0623bace3c53b04de3c7551f19
Jeff Parsons <parsoj@gmail.com>
2019-04-22 19:18:13 -0700

    add an empty URL

--- doom-emacs.rb
+++ doom-emacs.rb
@@ -2,6 +2,9 @@ class DoomEmacs < Formula
   desc "Installer for Doom Emacs"
   homepage "https://github.com/hlissner/doom-emacs"
 
+  url "file:///dev/null"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
   depends_on "emacs-plus --with-no-titlebar"
 
   def install
